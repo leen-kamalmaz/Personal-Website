125145745967bf0d9f7d19e21573b76fc02f639b
leen-kamalmaz <90856038+leen-kamalmaz@users.noreply.github.com>
2022-01-19 03:15:49 -0500

    Add files via upload

---
+++ contactPage.php
@@ -0,0 +1,16 @@
+<?php 
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $from = 'sarahkamalmaz@gmail.com'; 
+    $message = $_POST['message']; 
+    $subject = 'New Website Message'; 
+    $to = 'leenkamalmaz@gmail.com'; 
+    $body = "Name: $name\n".
+            "Email: $email\n".
+            "Message: $message\n";
+    $headers = "From: $from \r\n";
+    $headers .= "Reply-To: $email \r\n";
+    mail ($to, $subject, $body, $headers);
+
+    header("Location: contact.html");
+?>
